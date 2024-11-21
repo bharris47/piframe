9dd25edb6c4cbe86c55a10c8d1791c3a729a22a2
Ben Harris <benharris247@gmail.com>
2024-11-20 22:42:10 -0800

    add linux stuff

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo apt install -y gcc-arm-linux-gnueabihf libjpeg-dev swig liblgpio-dev
+sudo apt install -y gcc-arm-linux-gnueabihf libjpeg-dev swig liblgpio-dev libfreetype6-dev
 pip install -r requirements.txt
 pip install -e .
