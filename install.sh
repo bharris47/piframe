a0d369accbf6798ec7be473e684acf4fa62cd6e0
Ben Harris <benharris247@gmail.com>
2024-11-20 22:56:21 -0800

    system venv

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-sudo apt install -y gcc-arm-linux-gnueabihf libjpeg-dev swig liblgpio-dev libfreetype6-dev
-pip install virtualenv
+sudo apt update
+sudo apt install -y \
+  gcc-arm-linux-gnueabihf \
+  libjpeg-dev \
+  swig \
+  liblgpio-dev \
+  libfreetype6-dev \
+  python3-venv
 virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
