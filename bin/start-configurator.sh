15854abe20cc1cf3a143f9515cf162b4782051ae
Ben Harris <benharris247@gmail.com>
2024-11-21 23:37:26 -0800

    more streamlit conf

--- bin/start-configurator.sh
+++ bin/start-configurator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source .venv/bin/activate
-streamlit run --serverAddress 0.0.0.0 --serverPort 8080 piframe/app/configurator.py
+streamlit run --server.address, 0.0.0.0 --server.headless true --serverPort 8080 piframe/app/configurator.py
